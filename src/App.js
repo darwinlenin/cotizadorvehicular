4b2150973644f8a4b6ff7757df725d10e3f63356
ZoeGeop Technologies <zoegeop@gmail.com>
2021-02-17 09:28:23 -0500

    show cotization result

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import Resumen from './components/Resumen';
+import Resultado from './components/Resultado';
 import styled from '@emotion/styled';
 
 const Contenedor = styled.div`
@@ -27,7 +28,7 @@ function App() {
   });
 
   //extraer datos
-  const {datos} = resumen;
+  const {cotizacion, datos} = resumen;
 
   return (
     <Contenedor>
@@ -40,6 +41,9 @@ function App() {
         />
         <Resumen
           datos={datos}
+        />
+        <Resultado 
+          cotizacion={cotizacion}
         />
       </ContenedorFormulario>
     </Contenedor>
--- src/components/Resumen.js
+++ src/components/Resumen.js
@@ -1,4 +1,14 @@
 import React, {Fragment} from 'react';
+import {primerMayuscula} from '../helper';
+import styled from '@emotion/styled';
+
+const ContenedorResumen= styled.div`
+    padding:1rem;
+    text-align: center;
+    background-color: #00838F;
+    color: #FFF;
+    margin-top: 1rem;
+`;
 
 const Resumen = ({datos}) => {
 
@@ -9,15 +19,15 @@ const Resumen = ({datos}) => {
         return null;
 
     return ( 
-        <Fragment>
+        <ContenedorResumen>
             <h2>Resumen de Cotización</h2>
             <ul>
-                <li>Marca: </li>
-                <li>Plan: </li>
-                <li>Año del Auto: </li>
+                <li>Marca: {primerMayuscula(marca)} </li>
+                <li>Plan: {primerMayuscula(plan)} </li>
+                <li>Año del Auto: {year}</li>
             </ul>
-        </Fragment>
+        </ContenedorResumen>
      );
 }
- 
+
 export default Resumen;
